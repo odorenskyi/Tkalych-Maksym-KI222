1553569215558bfe6051624bae69f15af798d370
TkalychMaksim <tkalychmaksim@gmail.com>
2023-06-08 00:41:10 +0300

    Додана статична бібліотека для додатку

---
+++ lab09/prj/ModulesTkalych.h
@@ -0,0 +1,7 @@
+#ifndef MODULESTKALYCH_H_INCLUDED
+#define MODULESTKALYCH_H_INCLUDED
+float s_calculation(int x, int y, int z);
+float salary_calculate(int worked_days);
+int foot_size(int ukr_size);
+int count_binary_zeros_or_ones(int N);
+#endif // MODULESTKALYCH_H_INCLUDED
